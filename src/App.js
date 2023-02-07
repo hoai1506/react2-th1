ef8508b6c89cd07c4aadc51b1cf52baefbca942a
hoai1506 <luuhoai3107@gmail.com>
2023-02-07 19:51:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import photo1 from './photo1.jpg';
+import { base64Photo } from './Constant';
+import Logo from './logo.svg'; 
+import DeleteIcon from './DeleteIcon'; 
+import photo from './imgs/img.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>Đây là hướng dẫn dùng ảnh trong ReactJS</div>
+      <img src={photo} width="200" height="100" className='img' />
+      {/* <div className='require-title'>Day la anh dung require</div>
+      <img src={require('./photo1.jpg')} className='img2' />
+      <div className='require-title'>Đây là ảnh dùng link online</div>
+      <img src='https://i.imgur.com/0BJobQo.jpg' className='img2' />
+      <div className='require-title'>Đây là ảnh dùng base64</div>
+      <img src={base64Photo} className='img3' />
+      <div className='require-title'>Đây là ảnh dùng svg</div>
+      <img src={Logo} className='img3' />
+      <DeleteIcon /> */}
+      <div className='require-title'>Day la anh dung require</div>
+      <img src={require('./imgs/img.jpg')} className='img2' />
+      <div className='require-title'>Đây là ảnh dùng link online</div>
+      <img src='https://i.imgur.com/0BJobQo.jpg' className='img2' />
+      <div className='require-title'>Đây là ảnh dùng base64</div>
+      <img src={base64Photo} className='img3' />
+      <div className='require-title'>Đây là ảnh dùng svg</div>
+      <img src={Logo} className='img3' />
+    <DeleteIcon /> 
     </div>
   );
 }
